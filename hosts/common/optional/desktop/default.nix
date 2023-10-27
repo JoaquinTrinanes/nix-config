05b03f2f5921197c492c7be766540be79fe7d6a7
Joaquín Triñanes <hi@joaquint.io>
2023-10-27 08:00:57 +0200

    Remove redundant pkgs from desktop config

--- hosts/common/optional/desktop/default.nix
+++ hosts/common/optional/desktop/default.nix
@@ -3,29 +3,8 @@
   imports = [./gnome.nix ./stylix.nix ../audio.nix];
 
   environment.systemPackages = with pkgs; [
-    alejandra
     firefox
-    nil
-    git
     discord
-    wget
-    nil
-    rnix-lsp
-    nixfmt
-    gcc
-    libgcc
-    libcxx
-    stylua
-    ripgrep
-    sd
-    fd
-    fzf
-    gnumake
-    lua-language-server
-    pciutils
-    nix-index
-    lshw
-    unzip
   ];
   programs.dconf.enable = true;
 }
