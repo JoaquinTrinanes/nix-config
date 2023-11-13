236143c5c4c1c21d8b366ef2a738fc6e0513b1fb
Joaquín Triñanes <hi@joaquint.io>
2023-11-13 13:12:28 +0100

    Fix nushell autolaunching when opening nested bash instances

--- home-manager/nushell/default.nix
+++ home-manager/nushell/default.nix
@@ -34,9 +34,9 @@ in {
   ];
 
   programs.bash.initExtra = lib.mkAfter ''
-    if [[ $(ps --no-header --pid=$PPID --format=comm) != "nu" && -z ''${BASH_EXECUTION_STRING} ]]; then
-    	shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION='''
-    	exec "${config.programs.nushell.package}/bin/nu" "$LOGIN_OPTION"
-    fi
+    if [[ ! $(ps T --no-header --format=comm | grep "^nu$") && -z $BASH_EXECUTION_STRING ]]; then
+        shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION='''
+        exec "${config.programs.nushell.package}/bin/nu" "$LOGIN_OPTION"
+        fi
   '';
 }
