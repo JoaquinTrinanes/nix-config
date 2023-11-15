0a502337514176fdb86d63fc859435e9689065dd
Joaquín Triñanes <hi@joaquint.io>
2023-11-15 13:55:09 +0100

    Move huge dconf settings to separate file

---
+++ home-manager/dconf/default.nix
@@ -0,0 +1,60 @@
+{
+  lib,
+  pkgs,
+  ...
+}: {
+  dconf.settings = with lib.hm.gvariant; {
+    "org/gnome/desktop/peripherals/touchpad" = {
+      tap-to-click = true;
+      two-finger-scrolling-enabled = true;
+      natural-scroll = false;
+    };
+    "org/gnome/shell" = {
+      favorite-apps = ["org.gnome.Nautilus.desktop" "firefox.desktop" "discord.desktop" "org.wezfurlong.wezterm.desktop"];
+      enabled-extensions = with pkgs.gnomeExtensions;
+        builtins.map (extension: extension.extensionUuid) [
+          dash-to-panel
+          espresso
+          appindicator
+          night-theme-switcher
+        ];
+    };
+    "org/gnome/desktop/interface" = {
+      text-scaling-factor = 1.25;
+      show-battery-percentage = true;
+      enable-hot-corners = false;
+    };
+    "org/gnome/mutter" = {
+      edge-tiling = true;
+    };
+    "org/gnome/shell/extensions/espresso" = {
+      show-notifications = false;
+    };
+    "org/gnome/desktop/wm/preferences" = {
+      button-layout = "appmenu:minimize,maximize,close";
+    };
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
+      binding = "<Control><Alt>t";
+      command = "wezterm";
+      name = "Launch terminal";
+    };
+    "org/gnome/settings-daemon/plugins/media-keys" = {
+      custom-keybindings = ["/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"];
+    };
+    "org/gnome/desktop/wm/keybindings" = {
+      switch-windows = ["<Alt>Tab"];
+      switch-windows-backward = ["<Shift><Alt>Tab"];
+      switch-applications = ["<Super>Tab"];
+      switch-applications-backward = ["<Shift><Super>Tab"];
+    };
+    "org/gnome/desktop/input-sources" = {
+      sources = [(mkTuple ["xkb" "us"]) (mkTuple ["xkb" "es"])];
+    };
+    # "org/gnome/desktop/datetime" = {
+    #   automatic-timezone = true;
+    # };
+    # "org/gnome/system/location" = {
+    #   enabled = true;
+    # };
+  };
+}
