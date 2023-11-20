7738ad7687168a1ae7a3a7346140d85bb373fbb1
Joaquín Triñanes <hi@joaquint.io>
2023-11-21 00:16:06 +0100

    Support XBOOTLDR

--- hosts/razer-blade-14/hardware-configuration.nix
+++ hosts/razer-blade-14/hardware-configuration.nix
@@ -14,7 +14,7 @@
     ../common/optional/hardware-acceleration/amdgpu.nix
   ];
   boot.loader.systemd-boot.enable = true;
-  boot.loader.systemd-boot.configurationLimit = 1;
+  boot.loader.systemd-boot.configurationLimit = 10;
   boot.initrd.availableKernelModules = ["nvme" "xhci_pci" "thunderbolt" "usbhid" "usb_storage"];
   boot.initrd.kernelModules = ["dm-snapshot"];
   boot.kernelModules = ["kvm-amd"];
@@ -32,12 +32,23 @@
 
   boot.loader.efi.efiSysMountPoint = "/efi";
 
+  # XBOOTLDR support
+  fileSystems = {
+    "/efi/EFI/Linux" = {
+      device = "/boot/EFI/Linux";
+      options = ["bind"];
+    };
+    "/efi/EFI/nixos" = {
+      device = "/boot/EFI/nixos";
+      options = ["bind"];
+    };
+  };
+
   fileSystems."/efi" = {
     device = "/dev/nvme0n1p1";
   };
 
   fileSystems."/boot" = {
-    # fileSystems."/boot" = {
     device = "/dev/nvme0n1p7";
     fsType = "vfat";
   };
