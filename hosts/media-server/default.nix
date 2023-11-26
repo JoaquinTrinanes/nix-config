430a9f62f8e0e67a4116a3620c8ec7b69a65e528
Joaquín Triñanes <hi@joaquint.io>
2023-11-26 01:04:25 +0100

    Fix ssh server settings

--- hosts/media-server/default.nix
+++ hosts/media-server/default.nix
@@ -1,6 +1,19 @@
-{...}: {
+_: let
+  sshConfig = {
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII95dwUzUX98GxBzc13L/u/k+0rnZys6xDhNeEdkrsbq joaquin@razer-blade-14"
+    ];
+  };
+in {
   imports = [
     ./hardware-configuration.nix
     ../common/optional/ssh/server.nix
   ];
+
+  users.users."root" = sshConfig;
+  users.users."alfred" =
+    sshConfig
+    // {
+      isNormalUser = true;
+    };
 }
