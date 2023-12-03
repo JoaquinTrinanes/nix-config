9cde5f090277b62e9b25994c13cc04d254119da4
Joaquín Triñanes <hi@joaquint.io>
2023-12-03 13:11:42 +0100

    Enable keep-outputs, making rebuild much faster

--- hosts/common/global/default.nix
+++ hosts/common/global/default.nix
@@ -47,6 +47,7 @@
     trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
+    keep-outputs = true;
   };
 
   time.timeZone = lib.mkDefault "Europe/Madrid";
