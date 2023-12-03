bbb50f938818c367568d882401d33aa6a58d84de
Joaquín Triñanes <hi@joaquint.io>
2023-12-03 01:50:40 +0100

    Set en_DK as default locale

--- hosts/razer-blade-14/default.nix
+++ hosts/razer-blade-14/default.nix
@@ -52,6 +52,8 @@
 
   programs.nix-ld.enable = true;
 
+  i18n.defaultLocale = "en_DK.UTF-8";
+
   # This setups a SSH server. Very important if you're setting up a headless system.
   # Feel free to remove if you don't need it.
   # services.openssh = {
