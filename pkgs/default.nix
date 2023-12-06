d72131d9e63b03f46d57941d620a2409acd19d75
Joaquín Triñanes <hi@joaquint.io>
2023-12-07 00:28:17 +0100

    Remove custom nushell package

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,11 +1,5 @@
 # Custom packages, that can be defined similarly to ones from nixpkgs
 # You can build them using 'nix build .#example'
-pkgs: let
-  inherit (pkgs) callPackage;
-in {
-  nushell-nightly = callPackage ./nushell-nightly {
-    additionalFeatures = p: (p ++ ["extra" "dataframe"]);
-    doCheck = false;
-    inherit (pkgs.darwin.apple_sdk.frameworks) Libsystem Security AppKit;
-  };
+pkgs: {
+  # example: pkgs.callPackage ./example {};
 }
