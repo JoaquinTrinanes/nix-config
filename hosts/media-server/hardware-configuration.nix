177386e33430458eea507bede1d84c5726e9585b
Joaquín Triñanes <hi@joaquint.io>
2023-12-11 09:09:48 +0100

    Set boot timeout to 0 for media server

--- hosts/media-server/hardware-configuration.nix
+++ hosts/media-server/hardware-configuration.nix
@@ -10,6 +10,7 @@
   imports = [(modulesPath + "/installer/scan/not-detected.nix")];
 
   boot.loader.systemd-boot.enable = true;
+  boot.loader.timeout = 0;
   boot.initrd.availableKernelModules = ["xhci_pci" "ahci" "nvme" "sd_mod" "rtsx_usb_sdmmc"];
   boot.initrd.kernelModules = [];
   boot.kernelModules = ["kvm-intel"];
