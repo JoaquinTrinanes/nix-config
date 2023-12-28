4001762ccd6c244db0cf74f86b5d44919877ab2d
Joaquín Triñanes <hi@joaquint.io>
2023-12-28 02:26:40 +0100

    Add cuda binary cache

--- flake/configuration/substituters.nix
+++ flake/configuration/substituters.nix
@@ -2,6 +2,7 @@
   substituters = [
     "https://nix-community.cachix.org"
     "https://nushell-nightly.cachix.org"
+    "https://cuda-maintainers.cachix.org?priority=100"
     # "https://nix-shell.cachix.org"
     # "https://php-src-nix.cachix.org"
   ];
@@ -10,6 +11,7 @@
     "nushell-nightly.cachix.org-1:nLwXJzwwVmQ+fLKD6aH6rWDoTC73ry1ahMX9lU87nrc="
     "nix-shell.cachix.org-1:kat3KoRVbilxA6TkXEtTN9IfD4JhsQp1TPUHg652Mwc="
     "php-src-nix.cachix.org-1:3IMVbxfljrbI1NZjuML/2eHLsmHEXfzKGY0kEA20qWY="
+    "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
   ];
 in {
   nixos.sharedModules = [
