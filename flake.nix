facc66fe9d5d256807a74b435f9e228db30b5167
Joaquín Triñanes <hi@joaquint.io>
2023-12-28 14:15:54 +0100

    Make nix-colors follow nixpkgs

--- flake.nix
+++ flake.nix
@@ -11,6 +11,7 @@
     neovim-nightly-overlay.inputs.nixpkgs.follows = "nixpkgs";
 
     nix-colors.url = "github:misterio77/nix-colors";
+    nix-colors.inputs.nixpkgs-lib.follows = "nixpkgs";
 
     nix-index-database.url = "github:nix-community/nix-index-database";
     nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
