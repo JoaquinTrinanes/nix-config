74c03473bb00f57ba5a6f09d25e0bb0236b34dcf
Joaquín Triñanes <hi@joaquint.io>
2024-02-01 19:13:14 +0100

    Better filetype detection

--- home/users/joaquin/neovim/files/filetype.lua
+++ home/users/joaquin/neovim/files/filetype.lua
@@ -1,7 +1,16 @@
+local dotenv_type = "sh"
+
 vim.filetype.add({
-	extension = {},
-	filename = { ["flake.lock"] = "json", [".envrc"] = "sh" },
-	pattern = {
-		["%.env%.%a+"] = "sh",
-	},
+  extension = {
+    env = dotenv_type,
+    lock = "json",
+  },
+  filename = {
+    [".envrc"] = "sh",
+    [".env"] = dotenv_type,
+    ["env"] = dotenv_type,
+  },
+  pattern = {
+    ["%.env%.[%w_.-]+"] = dotenv_type,
+  },
 })
