f8e7431e3c9d60b5a3b4321abee57b4e6ef2a496
Joaquín Triñanes <hi@joaquint.io>
2024-02-28 13:12:08 +0100

    Remove substituters not needed for build from flake

--- flake.nix
+++ flake.nix
@@ -28,13 +28,11 @@
       "https://nix-community.cachix.org"
       "https://nushell-nightly.cachix.org"
       "https://cuda-maintainers.cachix.org"
-      "https://numtide.cachix.org"
     ];
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "nushell-nightly.cachix.org-1:nLwXJzwwVmQ+fLKD6aH6rWDoTC73ry1ahMX9lU87nrc="
       "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
-      "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
     ];
   };
 
