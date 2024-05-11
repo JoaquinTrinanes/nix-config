fdd2d0ec00f66d8190918cb901df33e6b31c3936
Joaquín Triñanes <hi@joaquint.io>
2024-05-11 13:49:11 +0200

    Conditionally add prime-run script

--- hosts/razer-blade-14/hardware-configuration.nix
+++ hosts/razer-blade-14/hardware-configuration.nix
@@ -71,13 +71,15 @@
   };
 
   environment.systemPackages = [
-    (pkgs.writeShellScriptBin "prime-run" ''
-      export __NV_PRIME_RENDER_OFFLOAD=1
-      export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
-      export __GLX_VENDOR_LIBRARY_NAME=nvidia
-      export __VK_LAYER_NV_optimus=NVIDIA_only
-      exec "$@"
-    '')
+    (lib.mkIf config.hardware.nvidia.prime.offload.enable (
+      pkgs.writeShellScriptBin "prime-run" ''
+        export __NV_PRIME_RENDER_OFFLOAD=1
+        export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
+        export __GLX_VENDOR_LIBRARY_NAME=nvidia
+        export __VK_LAYER_NV_optimus=NVIDIA_only
+        exec "$@"
+      ''
+    ))
   ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
