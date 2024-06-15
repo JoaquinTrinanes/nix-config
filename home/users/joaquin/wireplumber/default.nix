5300fe621c62bbc319ff455ca681b26d2acd718c
Joaquín Triñanes <hi@joaquint.io>
2024-06-15 23:43:13 +0200

    Fix wireplumber config

--- home/users/joaquin/wireplumber/default.nix
+++ home/users/joaquin/wireplumber/default.nix
@@ -1,19 +1,20 @@
-_: {
+{
   xdg.configFile = {
-    # "wireplumber/bluetooth.lua.d/51-bluez-config.lua".text = '''';
-    "wireplumber/main.lua.d/51-disable-microphone-output.lua".text = ''
-      rule = {
-        matches = {
-          {
-            { "node.name", "equals", "alsa_output.usb-the_t.bone_MB7_Beta_USB_the_t.bone_MB7_Beta_USB-00.analog-stereo" },
-          },
-        },
-        apply_properties = {
-          ["device.disabled"] = true,
-        },
-      }
-
-      table.insert(alsa_monitor.rules,rule)
+    "wireplumber/wireplumber.conf.d/51-disable-microphone-output.conf".text = ''
+      monitor.alsa.rules = [
+        {
+          matches = [
+            {
+              node.name = "alsa_output.usb-the_t.bone_MB7_Beta_USB_the_t.bone_MB7_Beta_USB-00.analog-stereo"
+            }
+          ],
+          actions = {
+            update-props = {
+              node.disabled = true
+            }
+          }
+        }
+      ]
     '';
   };
 }
