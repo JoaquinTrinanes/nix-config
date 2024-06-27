5b37cf82e6a9c9e03ee7ca9d63365bebfa6109a9
Joaquín Triñanes <hi@joaquint.io>
2024-06-27 13:25:06 +0200

    Don't launch steam with the dGPU

--- hosts/common/gaming/default.nix
+++ hosts/common/gaming/default.nix
@@ -1,1 +1,23 @@
-_: { programs.steam.enable = true; }
+{ pkgs, lib, ... }:
+let
+  steamPackages = pkgs.steamPackages.overrideScope (
+    _steamPkgsFinal: steamPkgsPrev: {
+      steam = steamPkgsPrev.steam.overrideAttrs (
+        _final: prev: {
+          postInstall = lib.concatLines [
+            prev.postInstall
+            ''
+              substituteInPlace $out/share/applications/steam.desktop \
+              --replace-fail PrefersNonDefaultGPU=true PrefersNonDefaultGPU=false \
+              --replace-fail X-KDE-RunOnDiscreteGpu=true X-KDE-RunOnDiscreteGpu=false
+            ''
+          ];
+        }
+      );
+    }
+  );
+in
+{
+  programs.steam.enable = true;
+  programs.steam.package = steamPackages.steam-fhsenv;
+}
