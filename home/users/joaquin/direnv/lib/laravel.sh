429d5e343e17cc55041de298c169ea251084f8ee
Joaquín Triñanes <hi@joaquint.io>
2024-07-04 10:13:09 +0200

    direnv: allow custom flake path in wrapper

--- home/users/joaquin/direnv/lib/laravel.sh
+++ home/users/joaquin/direnv/lib/laravel.sh
@@ -1,34 +1,34 @@
 #!/usr/bin/env bash
 
 layout_laravel_sail() {
-	layout php
+  layout php
 
-	lib_dir="$(direnv_layout_dir)/laravel_sail"
-	bin_dir="$lib_dir/bin"
-	base_dir="$(direnv_layout_dir)/.."
+  lib_dir="$(direnv_layout_dir)/laravel_sail"
+  bin_dir="$lib_dir/bin"
+  base_dir="$(direnv_layout_dir)/.."
 
-	rm -rf "$bin_dir"
+  rm -rf "$bin_dir"
 
-	if [ ! -f "$base_dir/vendor/bin/sail" ]; then
-		return
-	fi
+  if [ ! -f "$base_dir/vendor/bin/sail" ]; then
+    return
+  fi
 
-	mkdir -p "$bin_dir"
-	local scripts=(
-		composer
-		# php
-		artisan
-		tinker
-		# pint
-		# npm
-		# npx
-		# yarn
-		# pnpm
-	)
-	for script in "${scripts[@]}"; do
-		echo "\"$base_dir/vendor/bin/sail\" \"$script\" \"\$@\"" >"$bin_dir/$script"
-	done
+  mkdir -p "$bin_dir"
+  local scripts=(
+    composer
+    # php
+    artisan
+    tinker
+    # pint
+    # npm
+    # npx
+    # yarn
+    # pnpm
+  )
+  for script in "${scripts[@]}"; do
+    echo "\"$base_dir/vendor/bin/sail\" \"$script\" \"\$@\"" >"$bin_dir/$script"
+  done
 
-	chmod -R u+x "$bin_dir"
-	PATH_add "$bin_dir"
+  chmod -R u+x "$bin_dir"
+  PATH_add "$bin_dir"
 }
--- home/users/joaquin/direnv/lib/my-flake.sh
+++ home/users/joaquin/direnv/lib/my-flake.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
 
 use_my_flake() {
-	local PROJECT_NAME
-	PROJECT_NAME="$(basename "$(dirname "$(direnv_layout_dir)")")"
+  local PROJECT_NAME
+  PROJECT_NAME="$(basename "$PWD")"
 
-	FLAKE_PATH="$HOME/.flakes/$PROJECT_NAME"
+  if [ $# -ge 1 ]; then
+    PROJECT_NAME=$1
+    shift
+  fi
 
-	if [ -f "$FLAKE_PATH/flake.nix" ]; then
-		use flake "$FLAKE_PATH" --impure "$@"
-		export FLAKE_PATH
-	fi
+  FLAKE_PATH="$HOME/.flakes/$PROJECT_NAME"
+
+  if [ -f "$FLAKE_PATH/flake.nix" ]; then
+    use flake "$FLAKE_PATH" --impure "$@"
+    export FLAKE_PATH
+  fi
 }
