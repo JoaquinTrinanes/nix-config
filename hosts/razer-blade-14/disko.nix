cc78099f1e248256dfe9e69c5a5bc4bd4d4c81e6
Joaquín Triñanes <hi@joaquint.io>
2024-07-29 20:52:04 +0200

    disko: reduce nesting

--- hosts/razer-blade-14/disko.nix
+++ hosts/razer-blade-14/disko.nix
@@ -40,15 +40,15 @@
                   "--hash=sha512"
                 ];
                 postCreateHook = ''
-                  if [ "$(systemd-cryptenroll --fido2-device=list 2> /dev/null)" ]; then
+                  (
                     shouldTryEnroll=""
-                    read -r -p "FIDO2 device detected. Enroll? [y/N] " shouldTryEnroll
-                    if [[ "$shouldTryEnroll" =~ ^([yY][eE][sS]|[yY])$ ]]; then
-                      echo 'Remember to touch the key after entering the PIN!' >&2
-                      systemd-cryptenroll $device --fido2-device=auto  --fido2-with-client-pin=yes
+                    if [ "$(systemd-cryptenroll --fido2-device=list 2> /dev/null)" ]; then
+                      read -r -p "FIDO2 device detected. Enroll? [y/N] " shouldTryEnroll
+                      if [[ "$shouldTryEnroll" =~ ^([yY][eE][sS]|[yY])$ ]]; then
+                        systemd-cryptenroll $device --fido2-device=auto  --fido2-with-client-pin=yes
+                      fi
                     fi
-                    unset shouldTryEnroll
-                  fi
+                  )
                 '';
                 settings = { };
                 content = {
@@ -61,30 +61,73 @@
         };
       };
     };
-    lvm_vg = {
-      pool = {
-        type = "lvm_vg";
-        lvs = {
-          root = {
-            size = "100%FREE";
-            content = {
-              type = "btrfs";
-              # type="filesystem";
-              # format = "btrfs";
+    lvm_vg.pool = {
+      type = "lvm_vg";
+      lvs.root = {
+        size = "100%FREE";
+        content = {
+          type = "btrfs";
+          # mountpoint = "/";
+          extraArgs = [ "-f" ]; # Override existing partition
+          mountOptions = [ "defaults" ];
+          subvolumes = {
+            swap = {
+              mountpoint = "/.swapvol";
+              mountOptions = [
+                "defaults"
+                "noatime"
+              ];
+              swap.swapfile.size = "32G";
+            };
+            # persist = {
+            #   mountpoint = "/persist";
+            #   mountOptions = [
+            #     "defaults"
+            #     "noatime"
+            #     "compress=zstd"
+            #   ];
+            # };
+            nix = {
+              mountpoint = "/nix";
+              mountOptions = [
+                "defaults"
+                "noatime"
+                "compress=zstd"
+              ];
+            };
+            rootfs = {
               mountpoint = "/";
               mountOptions = [
                 "defaults"
-                "compress=zstd"
                 "noatime"
+                "compress=zstd"
               ];
-              subvolumes = {
-                "/swap" = {
-                  mountpoint = "/.swapvol";
-                  swap.swapfile.size = "32G";
-                };
-              };
+            };
+            home = {
+              mountpoint = "/home";
+              mountOptions = [
+                "defaults"
+                "noatime"
+                "compress=zstd"
+              ];
+            };
+            var-log = {
+              mountpoint = "/var/log";
+              mountOptions = [
+                "defaults"
+                "noatime"
+                "compress=zstd"
+              ];
             };
           };
+          postCreateHook = ''
+            (
+              MNTPOINT=$(mktemp -d)
+              mount "$device" "$MNTPOINT" -o subvol=/
+              trap 'umount "$MNTPOINT"; rm -rf "$MNTPOINT"' EXIT
+              btrfs subvolume snapshot -r $MNTPOINT/rootfs $MNTPOINT/rootfs-blank
+            )
+          '';
         };
       };
     };
