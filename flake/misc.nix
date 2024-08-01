1138d14d1696c910e9a90b0ef0633f688b88d050
Joaquín Triñanes <hi@joaquint.io>
2024-08-01 09:43:47 +0200

    Extract isLix var

--- flake/misc.nix
+++ flake/misc.nix
@@ -45,46 +45,50 @@
           { pkgs, config, ... }:
           {
             _file = ./misc.nix;
-            nix = {
-              package = lib.mkDefault pkgs.nix;
-              settings = {
-                allowed-users = lib.mkDefault [ "@wheel" ];
-                min-free = lib.mkDefault 128000000; # 128MB
-                max-free = lib.mkDefault 1000000000; # 1GB
-                connect-timeout = lib.mkDefault 5;
-                fallback = lib.mkDefault true;
-                log-lines = lib.mkDefault 50;
-                auto-optimise-store = lib.mkDefault true;
-                experimental-features = [
-                  "repl-flake"
-                  "nix-command"
-                  "flakes"
-                  "auto-allocate-uids"
-                  "ca-derivations"
-                  "no-url-literals"
-                ];
-                keep-outputs = lib.mkDefault true;
-                auto-allocate-uids = lib.mkDefault true;
-                narinfo-cache-negative-ttl = lib.mkDefault 0;
-                use-xdg-base-directories = lib.mkDefault true;
-                warn-dirty = lib.mkDefault false;
-                # remove global registry
-                flake-registry = lib.mkDefault "";
+            nix =
+              let
+                isLix = lib.getName config.nix.package == "lix";
+              in
+              {
+                package = lib.mkDefault pkgs.nix;
+                settings = {
+                  allowed-users = lib.mkDefault [ "@wheel" ];
+                  min-free = lib.mkDefault 128000000; # 128MB
+                  max-free = lib.mkDefault 1000000000; # 1GB
+                  connect-timeout = lib.mkDefault 5;
+                  fallback = lib.mkDefault true;
+                  log-lines = lib.mkDefault 50;
+                  auto-optimise-store = lib.mkDefault true;
+                  experimental-features = [
+                    "repl-flake"
+                    "nix-command"
+                    "flakes"
+                    "auto-allocate-uids"
+                    "ca-derivations"
+                    "no-url-literals"
+                  ];
+                  keep-outputs = lib.mkDefault true;
+                  auto-allocate-uids = lib.mkDefault true;
+                  narinfo-cache-negative-ttl = lib.mkDefault 0;
+                  use-xdg-base-directories = lib.mkDefault true;
+                  warn-dirty = lib.mkDefault false;
+                  # remove global registry
+                  flake-registry = lib.mkDefault "";
 
-                repl-overlays = lib.mkIf (lib.getName config.nix.package == "lix") [
-                  (pkgs.writeText "pkgs.nix"
-                    # nix
-                    ''
-                      info: final: prev:
-                      if prev ? legacyPackages && prev.legacyPackages ? ''${info.currentSystem} then
-                        { pkgs = prev.legacyPackages.''${info.currentSystem}; }
-                      else
-                        { }
-                    ''
-                  )
-                ];
+                  repl-overlays = lib.mkIf isLix [
+                    (pkgs.writeText "pkgs.nix"
+                      # nix
+                      ''
+                        info: final: prev:
+                        if prev ? legacyPackages && prev.legacyPackages ? ''${info.currentSystem} then
+                          { pkgs = prev.legacyPackages.''${info.currentSystem}; }
+                        else
+                          { }
+                      ''
+                    )
+                  ];
+                };
               };
-            };
           }
         )
       ];
