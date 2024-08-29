727861828b6afe5edc185373ecde1af628f980d1
Joaquín Triñanes <hi@joaquint.io>
2024-08-29 21:20:59 +0200

    neovim: move extras to config
    That way the (always changing) lazyvim.json file can be untracked

--- home/users/joaquin/neovim/default.nix
+++ home/users/joaquin/neovim/default.nix
@@ -52,7 +52,6 @@ in
     recursive = true;
   };
   xdg.configFile."nvim/lazy-lock.json".source = mkImpureLink ./files/lazy-lock.json;
-  xdg.configFile."nvim/lazyvim.json".source = mkImpureLink ./files/lazyvim.json;
   xdg.configFile."nvim/ftplugin" = {
     source = ./files/ftplugin;
     recursive = true;
