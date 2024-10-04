953df4c5e0b95ac45c313b185dd61e08f5d6912c
Joaquín Triñanes <hi@joaquint.io>
2024-10-04 11:53:57 +0200

    neovim: Highlight textwidth

--- home/users/joaquin/neovim/files/lua/config/options.lua
+++ home/users/joaquin/neovim/files/lua/config/options.lua
@@ -1,12 +1,16 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+local opt = vim.opt
 
-vim.opt.completeopt = "menuone,longest"
-vim.opt.wrap = true
-vim.opt.linebreak = true
--- vim.opt.signcolumn = "auto:2"
-vim.opt.smoothscroll = true
+opt.number = true
+opt.relativenumber = true
+
+opt.completeopt = "menuone,longest"
+
+opt.wrap = true
+opt.linebreak = true
+opt.smoothscroll = true
 
 vim.g.root_spec = { "cwd", "lsp", { ".git", "lua" } }
 
@@ -15,3 +19,14 @@ vim.g.root_spec = { "cwd", "lsp", { ".git", "lua" } }
 
 -- LSP Server to use for PHP.
 vim.g.lazyvim_php_lsp = "intelephense" -- or "phpactor"
+
+if vim.env.COLORTERM == nil then
+  opt.pumblend = 0 -- Popup blend
+  opt.termguicolors = false
+end
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Highlight one character after textwidth
+vim.opt.colorcolumn = "+1"
