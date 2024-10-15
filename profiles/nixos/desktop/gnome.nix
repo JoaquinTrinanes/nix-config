93a64507cf94230ef61c138c64d46a8c6e75d6a6
Joaquín Triñanes <hi@joaquint.io>
2024-10-16 00:17:02 +0200

    gnome: Fix default

--- profiles/nixos/desktop/gnome.nix
+++ profiles/nixos/desktop/gnome.nix
@@ -16,52 +16,51 @@ in
     enable = lib.mkEnableOption "gnome desktop profile";
   };
 
-  config = lib.mkIf cfg.enable (
-    lib.mkDefault {
-      services.xserver = {
-        desktopManager.gnome.enable = true;
-        desktopManager.gnome.sessionPath = [
-          inputs.self.packages.${pkgs.stdenv.hostPlatform.system}.dynamic-gnome-wallpapers
-        ];
-      };
-      programs.dconf.enable = true;
-      services.udev.packages = with pkgs; [ gnome.gnome-settings-daemon ];
+  config = lib.mkIf cfg.enable {
+    services.xserver = {
+      desktopManager.gnome.enable = lib.mkDefault true;
+      desktopManager.gnome.sessionPath = [
+        inputs.self.packages.${pkgs.stdenv.hostPlatform.system}.dynamic-gnome-wallpapers
+      ];
+    };
+    programs.dconf.enable = lib.mkDefault true;
+    services.udev.packages = with pkgs; [ gnome-settings-daemon ];
 
-      environment.gnome.excludePackages = builtins.attrValues {
-        inherit (pkgs)
-          gnome-connections
-          gnome-contacts
-          gnome-tour
-          # baobab # disk usage analyzer
-          # cheese # photo booth
-          # eog # image viewer
-          # gedit # text editor
-          totem # video player
-          yelp # help viewer
-          # evince # document viewer
-          # file-roller # archive manager
-          geary # email client
-          # seahorse # password manager
-          # gnome-font-viewer
-          gnome-logs
-          gnome-maps
-          gnome-music
-          gnome-weather
+    environment.gnome.excludePackages = builtins.attrValues {
+      inherit (pkgs)
+        gnome-connections
+        gnome-contacts
+        gnome-tour
+        # baobab # disk usage analyzer
+        # cheese # photo booth
+        # eog # image viewer
+        # gedit # text editor
+        totem # video player
+        yelp # help viewer
+        # evince # document viewer
+        # file-roller # archive manager
+        geary # email client
+        # seahorse # password manager
+        # gnome-font-viewer
+        gnome-logs
+        gnome-maps
+        gnome-music
+        gnome-weather
 
-          # gnome-calculator
-          # gnome-calendar
-          # gnome-characters
-          # gnome-clocks
-          # gnome-disk-utility
-          # gnome-photos
-          # gnome-screenshot
-          # gnome-system-monitor
-          ;
-      };
+        # gnome-calculator
+        # gnome-calendar
+        # gnome-characters
+        # gnome-clocks
+        # gnome-disk-utility
+        # gnome-photos
+        # gnome-screenshot
+        # gnome-system-monitor
+        ;
+    };
 
-      environment.systemPackages = builtins.attrValues {
-        inherit (pkgs) pinentry-gnome3 gnome-tweaks adwaita-icon-theme;
-      };
+    environment.systemPackages = builtins.attrValues {
+      inherit (pkgs) pinentry-gnome3 gnome-tweaks adwaita-icon-theme;
+    };
 
     # TODO: see if this works after reboot (ensure .config/mimeapps.list doesn't exist)
     xdg.mime.defaultApplications = {
