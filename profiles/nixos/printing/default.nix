a232c34038c81bf315d119413043e877cc777192
Joaquín Triñanes <hi@joaquint.io>
2025-03-18 20:29:42 +0100

    Fix IPP printing

--- profiles/nixos/printing/default.nix
+++ profiles/nixos/printing/default.nix
@@ -10,6 +10,18 @@ in
   config = lib.mkIf cfg.enable {
     services.printing = {
       enable = lib.mkDefault true;
+      browsing = lib.mkDefault true;
+      browsed.enable = lib.mkDefault true;
+      startWhenNeeded = lib.mkDefault true;
+      stateless = lib.mkDefault true;
+      browsedConf = ''
+        CreateIPPPrinterQueues All
+        UseCUPSGeneratedPPDs Yes
+      '';
+    };
+    services.avahi = {
+      enable = lib.mkDefault true;
+      nssmdns4 = lib.mkDefault true;
     };
   };
 }
