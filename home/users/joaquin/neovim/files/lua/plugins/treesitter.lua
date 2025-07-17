2dfbf9a051bf00b34b93cc203481dbdf0f55f887
Joaquín Triñanes <hi@joaquint.io>
2025-07-17 13:22:39 +0200

    vim: fix pure mode TS parsers

--- home/users/joaquin/neovim/files/lua/plugins/treesitter.lua
+++ home/users/joaquin/neovim/files/lua/plugins/treesitter.lua
@@ -1,26 +1,39 @@
 local U = require("config.util")
+
+---@type string|boolean
+local build_treesitter = ":TSUpdate"
+
+local ts_parser_install_dir = nil
+
+if vim.g.usePluginsFromStore then
+  build_treesitter = false
+  ts_parser_install_dir = vim.fn.stdpath("data") .. "/treesitter-parsers"
+  vim.opt.runtimepath:prepend(ts_parser_install_dir)
+end
+
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
+    build = build_treesitter,
     event = { "BufReadPost", "VeryLazy" },
     lazy = vim.fn.argc(-1) == 0, -- load treesitter early when opening a file from the cmdline
     cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
-    -- init = function(plugin)
-    --   -- PERF: add nvim-treesitter queries to the rtp and it's custom query predicates early
-    --   -- This is needed because a bunch of plugins no longer `require("nvim-treesitter")`, which
-    --   -- no longer trigger the **nvim-treesitter** module to be loaded in time.
-    --   -- Luckily, the only things that those plugins need are the custom queries, which we make available
-    --   -- during startup.
-    --   require("lazy.core.loader").add_to_rtp(plugin)
-    --   require("nvim-treesitter.query_predicates")
-    -- end,
+    init = function(plugin)
+      -- PERF: add nvim-treesitter queries to the rtp and it's custom query predicates early
+      -- This is needed because a bunch of plugins no longer `require("nvim-treesitter")`, which
+      -- no longer trigger the **nvim-treesitter** module to be loaded in time.
+      -- Luckily, the only things that those plugins need are the custom queries, which we make available
+      -- during startup.
+      require("lazy.core.loader").add_to_rtp(plugin)
+      require("nvim-treesitter.query_predicates")
+    end,
     keys = {
       { "<c-space>", desc = "Increment Selection" },
       { "<bs>", desc = "Decrement Selection", mode = "x" },
     },
     opts_extend = { "ensure_installed" },
     opts = {
+      parser_install_dir = ts_parser_install_dir,
       highlight = { enable = true },
       indent = { enable = true },
       ensure_installed = {
@@ -71,31 +84,15 @@ return {
     config = function(_, opts)
       if type(opts.ensure_installed) == "table" then
         opts.ensure_installed = U.dedup(opts.ensure_installed)
+        local parsers = require("nvim-treesitter.parsers").available_parsers()
+        opts.ensure_installed = vim
+          .iter(opts.ensure_installed)
+          :filter(function(lang)
+            return not vim.tbl_contains(parsers, lang)
+          end)
+          :totable()
       end
       require("nvim-treesitter.configs").setup(opts)
-    end,
-  },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-      if vim.g.usePluginsFromStore then
-        opts.ensure_installed = {}
-        return
-      end
-
-      local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-      parser_config["blade"] = {
-        install_info = {
-          url = "https://github.com/EmranMR/tree-sitter-blade",
-          files = { "src/parser.c" },
-          branch = "main",
-        },
-        filetype = "blade",
-      }
-
-      if type(opts.ensure_installed) == "table" then
-        vim.list_extend(opts.ensure_installed, { "blade", "php", "php_only", "html", "css" })
-      end
     end,
   },
   {
