b91890c97a701bcfcd3f9228b417bbcee987ad3b
Joaquín Triñanes <hi@joaquint.io>
2025-07-29 11:34:39 +0200

    vim: consistent lazydev config

--- home/users/joaquin/neovim/files/lua/plugins/coding.lua
+++ home/users/joaquin/neovim/files/lua/plugins/coding.lua
@@ -101,10 +101,31 @@ return {
     ft = "lua",
     cmd = "LazyDev",
     opts_extend = { "library" },
+    specs = {
+      {
+        "saghen/blink.cmp",
+        optional = true,
+        ---@module 'blink.cmp'
+        ---@type blink.cmp.Config
+        opts = {
+          sources = {
+            per_filetype = { lua = { inherit_defaults = true, "lazydev" } },
+            providers = {
+              lazydev = {
+                name = "LazyDev",
+                module = "lazydev.integrations.blink",
+                score_offset = 100, -- show at a higher priority than lsp
+              },
+            },
+          },
+        },
+      },
+    },
     opts = {
       library = {
         { path = "${3rd}/luv/library", words = { "vim%.uv" } },
         { path = "snacks.nvim", words = { "Snacks" } },
+        { path = "lazy.nvim" },
       },
     },
   },
