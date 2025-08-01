4542d32926744b2251086e60cf3e3fec6a751e00
Joaquín Triñanes <hi@joaquint.io>
2025-08-01 22:32:35 +0200

    wrapper-manager: show correct error location

--- flake/lib/default.nix
+++ flake/lib/default.nix
@@ -3,7 +3,21 @@
   parts.nixpkgs.overlays = [
     (final: _prev: {
       my = {
-        mkWrapper = inputs.wrapper-manager.lib.wrapWith final;
+        mkWrapper =
+          module:
+          let
+            finalModule =
+              if builtins.isAttrs module && (builtins.length (builtins.attrNames module)) > 0 then
+                {
+                  # show the actual file that defines the wrapper in case of error
+                  _file = (builtins.unsafeGetAttrPos (lib.head (builtins.attrNames module)) module).file;
+                }
+                // module
+
+              else
+                module;
+          in
+          inputs.wrapper-manager.lib.wrapWith final finalModule;
       };
     })
   ];
