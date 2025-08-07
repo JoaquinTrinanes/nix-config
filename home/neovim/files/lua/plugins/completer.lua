ccde01bf1bfd791619a75f8eef330949284af44a
Joaquín Triñanes <hi@joaquint.io>
2025-08-08 00:12:43 +0200

    vim: more consistent custom snippets location

--- home/neovim/files/lua/plugins/completer.lua
+++ home/neovim/files/lua/plugins/completer.lua
@@ -29,7 +29,9 @@ return {
             "rafamadriz/friendly-snippets",
             config = function()
               require("luasnip.loaders.from_vscode").lazy_load()
-              require("luasnip.loaders.from_vscode").lazy_load({ paths = { vim.fn.stdpath("config") .. "/snippets" } })
+              require("luasnip.loaders.from_vscode").lazy_load({
+                paths = vim.api.nvim_get_runtime_file("snippets", true),
+              })
             end,
           },
         },
