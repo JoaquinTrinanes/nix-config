a0bd44404676ea0338a4a3a351e1538e8be6b0a8
Joaquín Triñanes <hi@joaquint.io>
2025-08-14 14:54:32 +0200

    vim: add keymap to select and execute lsp commands

--- home/neovim/files/lua/config/util.lua
+++ home/neovim/files/lua/config/util.lua
@@ -52,6 +52,78 @@ function U.lsp.on_attach(on_attach, name)
   })
 end
 
+---@class PickLSPCommandOpts
+---@field prompt_for_args boolean
+
+---Pick and execute an LSP server command from any client
+---@param opts PickLSPCommandOpts?
+function U.lsp.select_command(opts)
+  ---@alias CmdOption { client: vim.lsp.Client, command: string }
+  opts = opts or {}
+  opts = vim.tbl_deep_extend("force", { prompt_for_args = false }, opts)
+
+  -- Get all LSP clients attached to the current buffer
+  local clients = vim.lsp.get_clients({ bufnr = 0 })
+
+  ---@type CmdOption[]
+  local all_commands = {}
+  for _, client in ipairs(clients) do
+    if client:supports_method("workspace/executeCommand") then
+      local cmds = client.server_capabilities.executeCommandProvider
+      if cmds and cmds.commands then
+        for _, c in ipairs(cmds.commands) do
+          vim.list_extend(all_commands, { { client = client, command = c } })
+        end
+      end
+    end
+  end
+
+  if #all_commands == 0 then
+    vim.notify("No executable commands available from attached clients", vim.log.levels.INFO, { title = "LSP" })
+    return
+  end
+
+  vim.ui.select(
+    all_commands,
+    {
+      prompt = "Select LSP command to run",
+      ---@param item CmdOption
+      format_item = function(item)
+        return string.format("[%s] %s", item.client.name, item.command)
+      end,
+    },
+    ---@param choice CmdOption
+    function(choice)
+      if choice then
+        local args = {}
+        if opts.prompt_for_args then
+          local input = vim.fn.input("Arguments (comma-separated, leave empty if none): ")
+          args = vim.split(input, "%s*,%s*", { trimempty = true })
+        end
+        choice.client:exec_cmd(
+          { command = choice.command, arguments = args },
+          { bufnr = 0 },
+          function(err, result, context, config)
+            if err then
+              vim.notify(err.message, vim.log.levels.ERROR, { title = "LSP" })
+              return
+            end
+            if result then
+              vim.notify(vim.inspect(result), vim.log.levels.INFO, { title = "LSP" })
+              return
+            end
+            vim.notify(
+              string.format("%s finished without output", context.method),
+              vim.log.levels.INFO,
+              { title = "LSP" }
+            )
+          end
+        )
+      end
+    end
+  )
+end
+
 ---@param plugin_name string
 function U.opts(plugin_name)
   local plugin = require("lazy.core.config").spec.plugins[plugin_name]
