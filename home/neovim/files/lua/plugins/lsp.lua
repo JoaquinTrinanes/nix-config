b83f06f4043c500fe703458d6828e867f84c77fb
Joaquín Triñanes <hi@joaquint.io>
2025-08-14 14:55:14 +0200

    vim: full LSP method path

--- home/neovim/files/lua/plugins/lsp.lua
+++ home/neovim/files/lua/plugins/lsp.lua
@@ -194,7 +194,7 @@ return {
       local Keys = require("config.lsp-keymaps").get()
       -- stylua: ignore
       vim.list_extend(Keys, {
-        { "gd", function() Snacks.picker.lsp_definitions() end, desc = "Goto Definition", has = "definition" },
+        { "gd", function() Snacks.picker.lsp_definitions() end, desc = "Goto Definition", has = "textDocument/definition" },
         { "gr", function() Snacks.picker.lsp_references() end, nowait = true, desc = "References" },
         { "gI", function() Snacks.picker.lsp_implementations() end, desc = "Goto Implementation" },
         { "gy", function() Snacks.picker.lsp_type_definitions() end, desc = "Goto T[y]pe Definition" },
