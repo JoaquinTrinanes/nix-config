9e00acadec6d59efd5f2e954cc1ec1034b09df1d
Joaquín Triñanes <hi@joaquint.io>
2025-08-18 17:18:56 +0200

    autofirma: fix firefox integration

--- profiles/nixos/autofirma/default.nix
+++ profiles/nixos/autofirma/default.nix
@@ -8,7 +8,7 @@ let
   cfg = config.profiles.autofirma;
 in
 {
-  imports = [ inputs.autofirma-nix.nixosModules.autofirma ];
+  imports = [ inputs.autofirma-nix.nixosModules.default ];
 
   options.profiles.autofirma = {
     enable = lib.mkEnableOption "autofirma profile";
@@ -17,15 +17,11 @@ in
   config = lib.mkIf cfg.enable {
     programs.autofirma = lib.mkDefault {
       enable = true;
-      firefoxIntegration.enable = false;
+      firefoxIntegration.enable = true;
     };
-
-    programs.firefox.preferences = {
-      "network.protocol-handler.app.afirma" = lib.mkDefault (
-        lib.getExe config.programs.autofirma.package
-      );
-      # "network.protocol-handler.warn-external.afirma" = false;
-      "network.protocol-handler.external.afirma" = lib.mkDefault true;
+    programs.configuradorfnmt = {
+      enable = true;
+      firefoxIntegration.enable = true;
     };
   };
 }
