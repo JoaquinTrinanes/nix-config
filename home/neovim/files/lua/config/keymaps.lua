9f9cb3d116157c3b55111a0ee7f7d6d5936a4be9
Joaquín Triñanes <hi@joaquint.io>
2025-10-07 16:10:48 +0200

    vim: simplify diagnostic mapping

--- home/neovim/files/lua/config/keymaps.lua
+++ home/neovim/files/lua/config/keymaps.lua
@@ -161,9 +161,12 @@ map("n", "]q", vim.cmd.cnext, { desc = "Next Quickfix" })
 
 -- diagnostic
 local diagnostic_goto = function(next, severity)
-  severity = severity and vim.diagnostic.severity[severity] or nil
   return function()
-    vim.diagnostic.jump({ count = next and 1 or -1, float = true, severity = severity })
+    vim.diagnostic.jump({
+      count = (next and 1 or -1) * vim.v.count1,
+      severity = severity and vim.diagnostic.severity[severity] or nil,
+      float = true,
+    })
   end
 end
 map("n", "<leader>cd", vim.diagnostic.open_float, { desc = "Line Diagnostics" })
