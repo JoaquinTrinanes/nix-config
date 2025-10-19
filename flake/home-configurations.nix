9cda70100d6678dcb5223babb5eb0605f13bc64b
Joaquín Triñanes <hi@joaquint.io>
2025-10-19 12:16:23 +0200

    Delete media-server and related config

--- flake/home-configurations.nix
+++ flake/home-configurations.nix
@@ -3,9 +3,6 @@
   parts = {
     users = {
       "joaquin" = {
-        sshPublicKeys = [
-          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDCutbKZk+mbku2/ndSociCACyV+Joc0QVYRfjxAHW79 openpgp:0x31C20393"
-        ];
         home-manager = {
           enable = true;
           modules = [ ../home ];
