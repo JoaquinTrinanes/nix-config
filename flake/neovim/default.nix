534abe6ad5e466ca9ef94315ea283b19dae62072
Joaquín Triñanes <hi@joaquint.io>
2026-01-11 15:01:33 +0100

    vim: remove textobjects workaround

--- flake/neovim/default.nix
+++ flake/neovim/default.nix
@@ -79,6 +79,7 @@
           nvim-navic
           nvim-nio
           nvim-treesitter-context
+          nvim-treesitter-textobjects
           nvim-ts-autotag
           omnisharp-extended-lsp-nvim
           persistence-nvim
@@ -95,18 +96,7 @@
           vim-dadbod-ui
           vim-sleuth
           which-key-nvim
-        ])
-        ++ [
-          (pkgs.vimPlugins.nvim-treesitter-textobjects.overrideAttrs (_: {
-            version = "2025-8-28";
-            src = builtins.fetchTree {
-              type = "github";
-              owner = "nvim-treesitter";
-              repo = "nvim-treesitter-textobjects";
-              rev = "1b2d85d3de6114c4bcea89ffb2cd1ce9e3a19931";
-            };
-          }))
-        ];
+        ]);
       extraPackages = builtins.attrValues {
         inherit (pkgs)
           basedpyright
