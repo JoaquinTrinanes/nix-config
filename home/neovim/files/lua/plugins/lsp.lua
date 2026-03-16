bbeff4249de34d4521089f954e764619db76706a
Joaquín Triñanes <hi@joaquint.io>
2026-03-16 17:59:45 +0100

    vim: fix codelens deprecation

--- home/neovim/files/lua/plugins/lsp.lua
+++ home/neovim/files/lua/plugins/lsp.lua
@@ -214,7 +214,7 @@ return {
         { "<C-k>", function() return vim.lsp.buf.signature_help() end, mode = "i", desc = "Signature Help", has = "textDocument/signatureHelp" },
         { "<leader>ca", vim.lsp.buf.code_action, desc = "Code Action", mode = { "n", "v" }, has = "textDocument/codeAction" },
         { "<leader>cc", vim.lsp.codelens.run, desc = "Run Codelens", mode = { "n", "v" }, has = "textDocument/codeLens" },
-        { "<leader>cC", vim.lsp.codelens.refresh, desc = "Refresh & Display Codelens", mode = { "n" }, has = "textDocument/codeLens" },
+        { "<leader>cC", vim.lsp.codelens.enable, desc = "Refresh & Display Codelens", mode = { "n" }, has = "textDocument/codeLens" },
         { "<leader>cR", function() Snacks.rename.rename_file() end, desc = "Rename File", mode ={"n"}, has = { "workspace/didRenameFiles", "workspace/willRenameFiles" } },
         { "<leader>cr", vim.lsp.buf.rename, desc = "Rename", has = "textDocument/rename" },
         { "<leader>cA", function()
